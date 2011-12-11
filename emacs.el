05eba324e1e58bd8c7f11d5b194be736751896d1
Takuya Murakami <tmurakam@tmurakam.org>
2011-12-11 23:52:21 +0900

    updated

--- emacs.el
+++ emacs.el
@@ -9,22 +9,6 @@
 ;; - First release.
 ;;
 
-(setq lang (getenv "LANG"))
-(cond
- ((equal lang "ja_JP.UTF-8")
-  (set-language-environment "Japanese")
-  (prefer-coding-system 'utf-8)
-  (set-buffer-file-coding-system 'utf-8)
-  (set-terminal-coding-system 'utf-8)
-  (set-keyboard-coding-system 'utf-8))
- (t
-  (progn
-    (set-language-environment "English")
-    ;;(set-default-coding-system 'iso-8859-1)
-    (set-buffer-file-coding-system 'iso8859-1)
-    (set-terminal-coding-system 'iso8859-1)
-    (set-keyboard-coding-system 'iso8859-1))))
-
 ;; Mule-UCS
 ;(require 'un-define)
 
@@ -52,25 +36,6 @@
   (if (file-exists-p file-dir) 
       (setq fast-lock-cache-directories (list file-dir))))
 
-;; Activate Backspace 
-(global-set-key "\C-h" 'delete-backward-char)
-(define-key function-key-map [delete] [8])
-(put 'delete 'ascii-character 8)
-
-;; Display time
-(display-time)
-
-;; Display line number
-(line-number-mode t)
-
-;; Display column number
-(column-number-mode t)
-
-;; Don't make new line when cursor is end of buffer.
-(setq next-line-add-newlines nil)
-
-;; Font-Lock mode
-(global-font-lock-mode t)
 
 ;; Use fast-lock-mode to display faster.
 ;; '10/9/21: この設定を使うと Java ファイルのオープン時にエラーになるためコメントアウト
@@ -89,16 +54,6 @@
 (defun up-a-lot   () (interactive) (scroll-up  ))
 (global-set-key [C-mouse-4] 'down-a-lot)
 (global-set-key [C-mouse-5] 'up-a-lot  )
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; key binds
-(global-set-key "\M-c" 'compile)
-(global-set-key "\M-g" 'goto-line)
-(global-set-key "\M-c" 'compile)
-;(global-set-key "\M-e" 'next-error)
-(global-set-key [up] 'previous-error)
-(global-set-key [down] 'next-error)
-(global-set-key  [(f1)]  (lambda () (interactive) (manual-entry (current-word))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Wanderlust
@@ -130,19 +85,6 @@
 (autoload 'navi2ch "navi2ch" "Navigator for 2ch for Emacs" t)
 (setq navi2ch-list-bbstable-url "http://menu.2ch.net/bbsmenu.html")
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ruby-mode
-(add-hook 'ruby-mode-hook
-          '(lambda ()
-             (inf-ruby-keys)
-             (setq tab-width 8)))
-(setq ruby-indent-level 2)
-
-(autoload 'rubydb "rubydb3x"
-  "run rubydb on program file in buffer *gud-file*.
-the directory containing file becomes the initial working directory
-and source-file directory for your debugger." t)
-
 ;; patch file
 (setq auto-mode-alist
       (append
@@ -156,24 +98,5 @@ and source-file directory for your debugger." t)
       (cons '("\\.pox?\\'" . po-mode) auto-mode-alist))
 (autoload 'po-mode "po-mode")
 
-
- (global-set-key [(f1)] (lambda () (interactive) (manual-
-       entry (current-word))))
-
-
-;;; use space for indentation
-(setq-default indent-tabs-mode nil)
-
-(custom-set-variables
-  ;; custom-set-variables was added by Custom -- don't edit or cut/paste it!
-  ;; Your init file should contain only one such instance.
- '(c-basic-offset 4)
- '(c-default-style (quote ((c-mode . "linux") (c++-mode . "linux") (objc-mode . "linux") (idl-mode . "linux") (pike-mode . "linux") (other . "linux"))))
- '(c-insert-tab-function (quote insert-tab))
- '(c-tab-always-indent nil)
- '(inhibit-local-menu-bar-menus t)
- '(perl-indent-level 4))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom -- don't edit or cut/paste it!
-  ;; Your init file should contain only one such instance.
- )
+(global-set-key [(f1)] (lambda () (interactive) (manual-
+  entry (current-word))))
--- init.el
+++ init.el
@@ -1,4 +1,77 @@
+;; Language settings
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
+;; Activate Backspace 
+(global-set-key "\C-h" 'delete-backward-char)
+(define-key function-key-map [delete] [8])
+(put 'delete 'ascii-character 8)
+
+;; key binds
+(global-set-key "\M-c" 'compile)
+(global-set-key "\M-g" 'goto-line)
+(global-set-key "\M-c" 'compile)
+;(global-set-key "\M-e" 'next-error)
+(global-set-key [up] 'previous-error)
+(global-set-key [down] 'next-error)
+;(global-set-key  [(f1)]  (lambda () (interactive) (manual-entry (current-word))))
+
+;; Display time
+(display-time)
+
+;; Display line number
+(line-number-mode t)
+
+;; Display column number
+(column-number-mode t)
+
+;; Don't make new line when cursor is end of buffer.
+(setq next-line-add-newlines nil)
+
+;; Font-Lock mode
+(global-font-lock-mode t)
+
+;; ruby-mode
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (inf-ruby-keys)
+             (setq tab-width 8)))
+(setq ruby-indent-level 2)
+
+(autoload 'rubydb "rubydb3x"
+  "run rubydb on program file in buffer *gud-file*.
+the directory containing file becomes the initial working directory
+and source-file directory for your debugger." t)
+
+;; auto install
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp"))
+(require 'auto-install)
+(setq auto-install-directory "~/.emacs.d/auto-install/")
+(auto-install-update-emacswiki-package-name t)
+(auto-install-compatibility-setup)
+
+;; system dependent configs
 (if (featurep 'meadow)
     (load-file "~/.emacs.d/meadow.el")
   (load-file "~/.emacs.d/emacs.el")
 )
+
+;;; use space for indentation
+(setq-default indent-tabs-mode nil)
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom -- don't edit or cut/paste it!
+  ;; Your init file should contain only one such instance.
+ '(c-basic-offset 4)
+ '(c-default-style (quote ((c-mode . "linux") (c++-mode . "linux") (objc-mode . "linux") (idl-mode . "linux") (pike-mode . "linux") (other . "linux"))))
+ '(c-insert-tab-function (quote insert-tab))
+ '(c-tab-always-indent nil)
+ '(inhibit-local-menu-bar-menus t)
+ '(perl-indent-level 4))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom -- don't edit or cut/paste it!
+  ;; Your init file should contain only one such instance.
+ )
