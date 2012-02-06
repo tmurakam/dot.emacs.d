e4562ff348c5a2d075ac34f9da884624e1e7fcf4
Takuya Murakami <tmurakam@tmurakam.org>
2012-02-06 21:53:25 +0900

    do not auto update autoinstall

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
 ;; auto-install
 (require 'auto-install)
 (setq auto-install-directory "~/.emacs.d/auto-install/")
-(auto-install-update-emacswiki-package-name t)
+;(auto-install-update-emacswiki-package-name t)
 (auto-install-compatibility-setup)             ; 互換性確保
 
 ;; load all files in .init.d
