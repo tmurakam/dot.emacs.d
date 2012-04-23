c6adafcb9e4dc8f42f6d450e498cebc336ab2e33
Takuya Murakami <tmurakam@tmurakam.org>
2012-04-23 16:17:32 +0900

    updated

--- init.d/mmm.el
+++ init.d/mmm.el
@@ -1,11 +1,13 @@
-(require 'mmm-auto)
-(setq mmm-global-mode 'maybe)
+(when (locate-library "mmm-auto")
+  (require 'mmm-auto)
+  (setq mmm-global-mode 'maybe)
 
-(mmm-add-classes
- '((embedded-css
-    :submode css-mode
-    :front "<style[^>]*>"
-    :back "</style>")))
-(mmm-add-mode-ext-class nil "\\.html\\'" 'embedded-css)
+  (mmm-add-classes
+   '((embedded-css
+      :submode css-mode
+      :front "<style[^>]*>"
+      :back "</style>")))
+  (mmm-add-mode-ext-class nil "\\.html\\'" 'embedded-css))
+
 
     
