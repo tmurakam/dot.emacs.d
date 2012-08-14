4aa99318a5ca5b8cb3d269bbcf3504bbed443579
Takuya Murakami <tmurakam@tmurakam.org>
2012-08-14 19:46:05 +0900

    updated

--- init.d/init-mmm.el
+++ init.d/init-mmm.el
@@ -7,7 +7,13 @@
       :submode css-mode
       :front "<style[^>]*>"
       :back "</style>")))
-  (mmm-add-mode-ext-class nil "\\.html\\'" 'embedded-css))
+  (mmm-add-mode-ext-class nil "\\.html\\'" 'embedded-css)
 
-
+  ;(mmm-add-classes
+  ; '((html-php
+  ;    :submode php-mode
+  ;    :front "<\\?php"
+  ;    :back "\\?>")))
+  ;(mmm-add-mode-ext-class nil "\\.php\\'" 'html-php)
+)
     
