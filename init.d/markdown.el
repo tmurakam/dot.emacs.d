83356c288fd959b8c030b56aad49550983901b03
Takuya Murakami <murakami@da.jp.nec.com>
2013-06-27 09:20:32 +0900

    updated

---
+++ init.d/markdown.el
@@ -0,0 +1,6 @@
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.txt\\'" . markdown-mode))
+
