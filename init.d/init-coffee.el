7246f356c16264712b014aac1731580168f8612e
Takuya Murakami <murakami@da.jp.nec.com>
2016-04-15 16:42:37 +0900

    update

--- init.d/init-coffee.el
+++ init.d/init-coffee.el
@@ -1,3 +1,3 @@
 (autoload 'coffee-mode "coffee-mode")
 (add-to-list 'auto-mode-alist '("\\.coffee\\'" . coffee-mode))
-(setq coffee-tab-width 2)
+(setq coffee-tab-width 4)
---
+++ init.d/init-nxml-mode.el
@@ -0,0 +1,6 @@
+(add-hook 'nxml-mode-hook
+          (lambda ()
+            (setq nxml-child-indent 4)
+            ))
+
+            
